af8a3194e3a42d947b3beef9437d958c0acf7621
bilimly <mathskairat@gmail.com>
2026-06-11 22:24:08 +0600

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,6 @@
 const { Pool } = require('pg');
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL || 'postgresql://postgres:ssVeJvDHblxFtjpznpCtzdGvOvJIdbHG@metro.proxy.rlwy.net:18612/railway',
+  connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false },
   max: 10,
   idleTimeoutMillis: 30000,
